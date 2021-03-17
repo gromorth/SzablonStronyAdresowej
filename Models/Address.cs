7cb4b700389e70f827bcd77b6810bdf6543065c9
gromorth <filip.roszkowski@wp.pl>
2021-03-18 00:06:28 +0100

    Dodaj pliki projektów.

---
+++ Models/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SzablonStrony.Models
+{
+    public class Address
+    {
+        [Display(Name = "Twoja ulubiona ulica:")]
+
+        public string Street { get; set; }
+        [Display(Name = "Twoj kod pocztowy:")]
+        public string ZipCode { get; set; }
+        [Display(Name = "Twoje miasto:")]
+        public string City { get; set; }
+        [Display(Name = "Twoj telefon:")]
+        public int Number { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SzablonStrony.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty(SupportsGet = true)]
+        public Models.Address Address { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Privacy");
+        }
+
+            public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {         
+                Name = "User,kaszka123";
+            }
+        }
+    }
+}
